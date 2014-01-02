4bd9d6e7481eb3a14ae2d4847132d48dd894cf8f
AlexJPZ17 <alexjpz17@gmail.com>
2014-01-01 23:14:50 -0600

    C Sjarp Unidadse de longitud

---
+++ Unidadse de longitud1/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace Unidadse_de_longitud
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ListBox_Medidas = new System.Windows.Forms.ListBox();
+            this.Button_Run = new System.Windows.Forms.Button();
+            this.Button_Clean = new System.Windows.Forms.Button();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.Label2 = new System.Windows.Forms.Label();
+            this.GroupBox1 = new System.Windows.Forms.GroupBox();
+            this.ComboBox_Opciones = new System.Windows.Forms.ComboBox();
+            this.TextBox_Valor = new System.Windows.Forms.TextBox();
+            this.GroupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ListBox_Medidas
+            // 
+            this.ListBox_Medidas.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.18868F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ListBox_Medidas.FormattingEnabled = true;
+            this.ListBox_Medidas.ItemHeight = 18;
+            this.ListBox_Medidas.Location = new System.Drawing.Point(6, 19);
+            this.ListBox_Medidas.Name = "ListBox_Medidas";
+            this.ListBox_Medidas.Size = new System.Drawing.Size(202, 202);
+            this.ListBox_Medidas.TabIndex = 0;
+            // 
+            // Button_Run
+            // 
+            this.Button_Run.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Button_Run.ForeColor = System.Drawing.SystemColors.Control;
+            this.Button_Run.Location = new System.Drawing.Point(18, 366);
+            this.Button_Run.Name = "Button_Run";
+            this.Button_Run.Size = new System.Drawing.Size(75, 23);
+            this.Button_Run.TabIndex = 6;
+            this.Button_Run.Text = "Run";
+            this.Button_Run.UseVisualStyleBackColor = true;
+            this.Button_Run.Click += new System.EventHandler(this.Button_Run_Click);
+            // 
+            // Button_Clean
+            // 
+            this.Button_Clean.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Button_Clean.ForeColor = System.Drawing.SystemColors.Control;
+            this.Button_Clean.Location = new System.Drawing.Point(154, 366);
+            this.Button_Clean.Name = "Button_Clean";
+            this.Button_Clean.Size = new System.Drawing.Size(75, 23);
+            this.Button_Clean.TabIndex = 8;
+            this.Button_Clean.Text = "Clean";
+            this.Button_Clean.UseVisualStyleBackColor = true;
+            this.Button_Clean.Click += new System.EventHandler(this.Button_Clean_Click);
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.Label1.Location = new System.Drawing.Point(128, 25);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(59, 15);
+            this.Label1.TabIndex = 7;
+            this.Label1.Text = "Opciones";
+            // 
+            // Label2
+            // 
+            this.Label2.AutoSize = true;
+            this.Label2.ForeColor = System.Drawing.SystemColors.Control;
+            this.Label2.Location = new System.Drawing.Point(27, 25);
+            this.Label2.Name = "Label2";
+            this.Label2.Size = new System.Drawing.Size(35, 15);
+            this.Label2.TabIndex = 9;
+            this.Label2.Text = "Valor";
+            // 
+            // GroupBox1
+            // 
+            this.GroupBox1.Controls.Add(this.ListBox_Medidas);
+            this.GroupBox1.Location = new System.Drawing.Point(12, 98);
+            this.GroupBox1.Name = "GroupBox1";
+            this.GroupBox1.Size = new System.Drawing.Size(217, 249);
+            this.GroupBox1.TabIndex = 10;
+            this.GroupBox1.TabStop = false;
+            // 
+            // ComboBox_Opciones
+            // 
+            this.ComboBox_Opciones.FormattingEnabled = true;
+            this.ComboBox_Opciones.Items.AddRange(new object[] {
+            "Milimetros",
+            "Centimetros",
+            "Pulgadas",
+            "Pies",
+            "Metros",
+            "Yardas",
+            "Kilometros",
+            "Millas"});
+            this.ComboBox_Opciones.Location = new System.Drawing.Point(108, 54);
+            this.ComboBox_Opciones.Name = "ComboBox_Opciones";
+            this.ComboBox_Opciones.Size = new System.Drawing.Size(121, 21);
+            this.ComboBox_Opciones.TabIndex = 4;
+            this.ComboBox_Opciones.Text = "Unidades a convertir";
+            // 
+            // TextBox_Valor
+            // 
+            this.TextBox_Valor.Location = new System.Drawing.Point(12, 54);
+            this.TextBox_Valor.Name = "TextBox_Valor";
+            this.TextBox_Valor.Size = new System.Drawing.Size(68, 20);
+            this.TextBox_Valor.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(33)))), ((int)(((byte)(33)))));
+            this.ClientSize = new System.Drawing.Size(242, 411);
+            this.Controls.Add(this.Button_Run);
+            this.Controls.Add(this.Button_Clean);
+            this.Controls.Add(this.Label1);
+            this.Controls.Add(this.Label2);
+            this.Controls.Add(this.GroupBox1);
+            this.Controls.Add(this.ComboBox_Opciones);
+            this.Controls.Add(this.TextBox_Valor);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.GroupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.ListBox ListBox_Medidas;
+        internal System.Windows.Forms.Button Button_Run;
+        internal System.Windows.Forms.Button Button_Clean;
+        internal System.Windows.Forms.Label Label1;
+        internal System.Windows.Forms.Label Label2;
+        internal System.Windows.Forms.GroupBox GroupBox1;
+        internal System.Windows.Forms.ComboBox ComboBox_Opciones;
+        internal System.Windows.Forms.TextBox TextBox_Valor;
+
+    }
+}
+
---
+++ Unidadse de longitud1/Form1.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Unidadse_de_longitud
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void UnidadesDeMedidas()
+        {
+            //Declaramos las variables 
+            double res1, res2, res3, res4, res5, res6, res7, valor;
+            string unidades;
+
+            //Condición que valida el TextBox Valor si  está vacío 
+            if (TextBox_Valor.Text == "")
+            {
+                MessageBox.Show("Ingrese un valor a convertir", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                //Coloca el Focus en el TextBox Valor
+                TextBox_Valor.Focus();
+
+            }
+            else
+            {
+                //Función que convierte en un tipo double el valor que se ingrese por 
+                //el TextBox Valor y lo almacena en la variable valor 
+                valor = Double.Parse(TextBox_Valor.Text);
+                //Método para extraer los Items seleccionado en el ComboBox
+                unidades = ComboBox_Opciones.Text;
+                /*Condición que evalúa la variable unidades si es igual a las funciones 
+                  Seleccionada en el ComboBox y si la variable unidades es igual a
+                  Unidades a convertir que muestre un mensaje*/
+                if (unidades == "Unidades a convertir")
+                {
+                    MessageBox.Show("Seleccione unas de las unidades a concertir", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                }
+                else
+                {
+
+                    /* Con la instrucción switch nos ayudara a ejecutar las opciones 
+                      seleccionada en el ComboBox de acuerdo a la unidad de medida 
+                      que escojamos */
+                    switch (unidades)
+                    {
+                       case "Milimetros":
+                           //limpiamos el ListBox antes de mostrar el resultado
+                            ListBox_Medidas.Items.Clear();
+                           //Medidas en milimetros
+                            res1 = valor * 0.1;
+                            res2 = valor * 0.03937;
+                            res3 = valor * 0.00328084;
+                            res4 = valor * 0.001;
+                            res5 = valor * 0.0010936;
+                            res6 = valor * 0.000001;
+                            res7 = valor * 0.0000006;
+                            ListBox_Medidas.Items.Add(res1 + " Centímetros");
+                            ListBox_Medidas.Items.Add(res2 + " Pulgadas");
+                            ListBox_Medidas.Items.Add(res3 + " Pies");
+                            ListBox_Medidas.Items.Add(res4 + " Metros");
+                            ListBox_Medidas.Items.Add(res5 + " Yardas");
+                            ListBox_Medidas.Items.Add(res6 + " Kilómetros");
+                            ListBox_Medidas.Items.Add(res7 + " Millas");
+                            break;
+
+                       case "Centimetros":
+                            //limpiamos el ListBox antes de que de el resultado
+                            ListBox_Medidas.Items.Clear();
+                            //Medidas en centímetros
+                            res1 = valor * 10.0;
+                            res2 = valor * 0.3937;
+                            res3 = valor * 0.0328084;
+                            res4 = valor * 0.01;
+                            res5 = valor * 0.010936;
+                            res6 = valor * 0.00001;
+                            res7 = valor * 0.000006;
+                            ListBox_Medidas.Items.Add(res1 + " Milímetros");
+                            ListBox_Medidas.Items.Add(res2 + " Pulgadas");
+                            ListBox_Medidas.Items.Add(res3 + " Pies");
+                            ListBox_Medidas.Items.Add(res4 + " Metros");
+                            ListBox_Medidas.Items.Add(res5 + " Yardas");
+                            ListBox_Medidas.Items.Add(res6 + " Kilómetros");
+                            ListBox_Medidas.Items.Add(res7 + " Millas");
+
+                            break;
+
+                       case "Pulgadas":
+                            //limpiamos el ListBox antes de que de el resultado
+                            ListBox_Medidas.Items.Clear();
+                            //Medias en pulgadas
+                            res1 = valor * 25.4;
+                            res2 = valor * 2.54;
+                            res3 = valor * 0.083;
+                            res4 = valor * 0.0254;
+                            res5 = valor * 0.027;
+                            res6 = valor * 0.0000254;
+                            res7 = valor * 0.0000158;
+                            ListBox_Medidas.Items.Add(res1 + " Milímetros");
+                            ListBox_Medidas.Items.Add(res2 + " Centimetros");
+                            ListBox_Medidas.Items.Add(res3 + " Pies");
+                            ListBox_Medidas.Items.Add(res4 + " Metros");
+                            ListBox_Medidas.Items.Add(res5 + " Yardas");
+                            ListBox_Medidas.Items.Add(res6 + " Kilómetros");
+                            ListBox_Medidas.Items.Add(res7 + " Millas");
+
+                            break;
+
+                       case "Pies":
+                            //limpiamos el ListBox antes de que de el resultado
+                            ListBox_Medidas.Items.Clear();
+                            //Medidas en pies 
+                            res1 = valor * 304.8;
+                            res2 = valor * 30.48;
+                            res3 = valor * 12;
+                            res4 = valor * 0.3048;
+                            res5 = valor * 0.33;
+                            res6 = valor * 0.0003048;
+                            res7 = valor * 0.0001894;
+                            ListBox_Medidas.Items.Add(res1 + " Milímetros");
+                            ListBox_Medidas.Items.Add(res2 + " Centímetros");
+                            ListBox_Medidas.Items.Add(res3 + " Pulgadas");
+                            ListBox_Medidas.Items.Add(res4 + " Metros");
+                            ListBox_Medidas.Items.Add(res5 + " Yardas");
+                            ListBox_Medidas.Items.Add(res6 + " Kilómetros");
+                            ListBox_Medidas.Items.Add(res7 + " Millas");
+
+                            break;
+
+                       case "Metros":
+                            //limpiamos el ListBox antes de que de el resultado
+                            ListBox_Medidas.Items.Clear();
+                            //Medidas en metros
+                            res1 = valor * 1000;
+                            res2 = valor * 100;
+                            res3 = valor * 39.37008;
+                            res4 = valor * 3.28084;
+                            res5 = valor * 1.09361;
+                            res6 = valor * 0.001;
+                            res7 = valor * 0.00062;
+                            ListBox_Medidas.Items.Add(res1 + " Milímetros");
+                            ListBox_Medidas.Items.Add(res2 + " Centímetros");
+                            ListBox_Medidas.Items.Add(res3 + " Pulgadas");
+                            ListBox_Medidas.Items.Add(res4 + " Pies");
+                            ListBox_Medidas.Items.Add(res5 + " Yardas");
+                            ListBox_Medidas.Items.Add(res6 + " Kilómetros");
+                            ListBox_Medidas.Items.Add(res7 + " Millas");
+
+                            break;
+
+                       case "Yardas":
+                            //limpiamos el ListBox antes de que de el resultado
+                            ListBox_Medidas.Items.Clear();
+                            //Medidas en yardas 
+                            res1 = valor * 914.4;
+                            res2 = valor * 91.44;
+                            res3 = valor * 36;
+                            res4 = valor * 3;
+                            res5 = valor * 0.9144;
+                            res6 = valor * 0.0009144;
+                            res7 = valor * 0.00056;
+                            ListBox_Medidas.Items.Add(res1 + " Milímetros");
+                            ListBox_Medidas.Items.Add(res2 + " Centímetros");
+                            ListBox_Medidas.Items.Add(res3 + " Pulgadas");
+                            ListBox_Medidas.Items.Add(res4 + " Pies");
+                            ListBox_Medidas.Items.Add(res5 + " Metros");
+                            ListBox_Medidas.Items.Add(res6 + " Kilómetros");
+                            ListBox_Medidas.Items.Add(res7 + " Millas");
+
+                            break;
+
+                       case "Kilometros":
+                            //limpiamos el ListBox antes de que de el resultado
+                            ListBox_Medidas.Items.Clear();
+                            //Medidas en kilómetros 
+                            res1 = valor * 1000000;
+                            res2 = valor * 100000;
+                            res3 = valor * 39370.08;
+                            res4 = valor * 3280.84;
+                            res5 = valor * 1000;
+                            res6 = valor * 1093.613;
+                            res7 = valor * 0.62137;
+                            ListBox_Medidas.Items.Add(res1 + " Milímetros");
+                            ListBox_Medidas.Items.Add(res2 + " Centímetros");
+                            ListBox_Medidas.Items.Add(res3 + " Pulgadas");
+                            ListBox_Medidas.Items.Add(res4 + " Pies");
+                            ListBox_Medidas.Items.Add(res5 + " Metros");
+                            ListBox_Medidas.Items.Add(res6 + " Yardas");
+                            ListBox_Medidas.Items.Add(res7 + " Millas");
+
+                            break;
+
+                       case "Millas":
+                            //limpiamos el ListBox antes de que de el resultado
+                            ListBox_Medidas.Items.Clear();
+                            //Medidas en millas 
+                            res1 = valor * 1609344;
+                            res2 = valor * 160934.4;
+                            res3 = valor * 63360;
+                            res4 = valor * 5280;
+                            res5 = valor * 1609.344;
+                            res6 = valor * 1760;
+                            res7 = valor * 1.60934;
+                            ListBox_Medidas.Items.Add(res1 + " Milímetros");
+                            ListBox_Medidas.Items.Add(res2 + " Centímetros");
+                            ListBox_Medidas.Items.Add(res3 + " Pulgadas");
+                            ListBox_Medidas.Items.Add(res4 + " Pies");
+                            ListBox_Medidas.Items.Add(res5 + " Metros");
+                            ListBox_Medidas.Items.Add(res6 + " Yardas");
+                            ListBox_Medidas.Items.Add(res7 + " Kilómetros");
+
+                            break;
+
+
+                    }
+                }
+            }
+        }
+
+        private void Button_Run_Click(object sender, EventArgs e)
+        {
+            UnidadesDeMedidas();
+        }
+
+        private void Button_Clean_Click(object sender, EventArgs e)
+        {
+            TextBox_Valor.Text = "";
+            TextBox_Valor.Focus();
+            ListBox_Medidas.Items.Clear();
+            ComboBox_Opciones.Text = "Unidades a convertir";
+
+        }
+    }
+}
